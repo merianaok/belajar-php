5518d7519882ad6161ec616e468f7e086a46aede
Meri Ana Oktaviya <meriana814@gmail.com>
2022-10-24 13:48:45 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+    echo "Hello World!<br>";
+
+    $nama = "Meri";
+    $umur = "20";
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaadik = "Alisa";
+    $umuradik = 6;
+
+    echo "Nama adik saya <strong>$namaadik</strong>, adik saya berusia $umuradik tahun.<br>";
+
+    $selisihumur = $umur-$umuradik;
+    echo "Selisih usia saya dengan adik saya adalah $selisihumur tahun";
+?>
